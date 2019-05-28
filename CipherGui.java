fa81917040f2123c5266934ea4b7ed068089549d
0xmmalik <manavmalik36@gmail.com>
2019-05-28 19:44:46 -0400

    added decoder for caesar cipher, added buggy vigenere cipher encoder, and paved the road for vigenere and rot13 ciphers

--- CipherGui.java
+++ CipherGui.java
@@ -7,11 +7,18 @@ import java.awt.event.ActionListener;
 public class CipherGui {
     private JTextField plainTextCaesar;
     private JButton encodeTextButton;
-    private JSpinner key;
+    private JSpinner keyCaesar;
     private JButton caesarLearn;
     private JPanel panelMain;
     private JTabbedPane tabbedPane1;
     private JLabel caesarCipherLabel;
+    private JFormattedTextField cipherTextCaesar;
+    private JSpinner deKeyCaesar;
+    private JButton decodeTextButton;
+    private JFormattedTextField keyVig;
+    private JFormattedTextField deKeyVig;
+    private JButton encodeVig;
+    private JFormattedTextField plainVig;
 
     public CipherGui() {
         caesarLearn.addActionListener(new ActionListener() {
@@ -24,7 +31,7 @@ public class CipherGui {
             public void actionPerformed(ActionEvent e) {
                 String encoded = "";
                 String original = plainTextCaesar.getText().toLowerCase();
-                int kei = (int)(key.getValue());
+                int kei = (int)(keyCaesar.getValue());
                 while(kei < 0)
                     kei += 26;
                 kei = kei % 26;
@@ -40,6 +47,63 @@ public class CipherGui {
 
                     else if(character > 'Z' && character < 'a')
                         character = (char)(character + 'A' - 'Z' - 1);
+
+                    encoded += character;
+                }
+                JOptionPane.showMessageDialog(null, encoded);
+            }
+        });
+
+        decodeTextButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String decoded = "";
+                String original = cipherTextCaesar.getText().toLowerCase();
+                int kei = (int)(deKeyCaesar.getValue());
+                kei = -kei;
+                while(kei < 0)
+                    kei += 26;
+                kei = kei % 26;
+
+                for(int i = 0; i < original.length(); i++) {
+                    char character = original.charAt(i);
+
+                    if(character >= 'a' && character <= 'z' || character >= 'A' && character <= 'Z')
+                        character = (char)(character + kei);
+
+                    if(character > 'z')
+                        character = (char)(character + 'a' - 'z' - 1);
+
+                    else if(character > 'Z' && character < 'a')
+                        character = (char)(character + 'A' - 'Z' - 1);
+
+                    decoded += character;
+                }
+                JOptionPane.showMessageDialog(null, decoded);
+            }
+        });
+
+        encodeVig.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String encoded = "";
+                String original = plainVig.getText().toLowerCase();
+                String kei = keyVig.getText().toLowerCase().replaceAll("\\W+", "");
+
+                for (int i = 0, j = 0; i < original.length(); i++, j += 0) {
+                    char character = original.charAt(i);
+                    char addition = kei.charAt(j);
+
+                    addition -= 'a';
+
+                    if (character >= 'a' && character <= 'z')
+                        character = (char) (character + addition);
+                        j = (j + 1) % kei.length();
+
+                    if (character > 'z')
+                        character = (char) (character + 'a' - 'z' - 1);
+
+                    else if (character > 'Z' && character < 'a')
+                        character = (char) (character + 'A' - 'Z' - 1);
 
                     encoded += character;
                 }
